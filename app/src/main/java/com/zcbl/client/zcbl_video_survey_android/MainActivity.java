e453c6e2d737b9c23d63ba5bf258204f7af89f03
Serenity那年 <wangyulu@zhongchebaolian.com>
2018-01-11 17:06:33 +0800

    初始化项目资源

--- app/src/main/java/com/zcbl/client/zcbl_video_survey_android/MainActivity.java
+++ app/src/main/java/com/zcbl/client/zcbl_video_survey_android/MainActivity.java
@@ -1,13 +1,28 @@
 package com.zcbl.client.zcbl_video_survey_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.zcbl.client.zcbl_video_survey_library.VideoSurveyConnectTransionActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.text);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, VideoSurveyConnectTransionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
