5d7b7f8546be99818b27db0f479cce03dc80a10c
Serenity那年 <wangyulu@zhongchebaolian.com>
2018-01-11 17:19:39 +0800

    添加注释

--- zcbl-video-survey-library/src/main/java/com/zcbl/client/zcbl_video_survey_library/VideoSurveyConnectTransionActivity.java
+++ zcbl-video-survey-library/src/main/java/com/zcbl/client/zcbl_video_survey_library/VideoSurveyConnectTransionActivity.java
@@ -6,10 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by serenitynanian on 2018/1/11.
+ * 视频查勘过渡页面
+ * 初始化查勘所需资源
  */
-
 public class VideoSurveyConnectTransionActivity extends AppCompatActivity {
 
+    /**
+     * 初始化生命周期方法
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
