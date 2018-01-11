942d22e3d2a93b87cf5921b8a2b3993a2a6c4cb3
Serenity那年 <wangyulu@zhongchebaolian.com>
2018-01-11 17:06:17 +0800

    初始化项目资源

---
+++ zcbl-video-survey-library/src/main/java/com/zcbl/client/zcbl_video_survey_library/VideoSurveyConnectTransionActivity.java
@@ -0,0 +1,18 @@
+package com.zcbl.client.zcbl_video_survey_library;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by serenitynanian on 2018/1/11.
+ */
+
+public class VideoSurveyConnectTransionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_survey_connect_transition);
+    }
+}
